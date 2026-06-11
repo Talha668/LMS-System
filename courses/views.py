a3d43805f9335dab8d8c82b9e8371ed1a70aa0f9
Ali Talha <alit62364@gmail.com>
2026-06-11 22:14:30 +0500

    removed duplicate timezone in courses/views.py

--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,3 @@
-from datetime import timezone
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
